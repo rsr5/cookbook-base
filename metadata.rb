8bc743d3613646213d501a1c8c328694cb35f044
Robin Ridler <robin.ridler@gmail.com>
2016-03-31 12:19:49 +0100

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'robin.ridler@gmail.com'
 license 'all_rights'
 description 'Installs/Configures base'
 long_description 'Installs/Configures base'
-version '0.1.0'
+version '0.1.1'
 
 depends 'sudo'
 depends 'poise-python'
