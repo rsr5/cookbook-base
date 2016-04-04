c90f1ff2737d6082475a1a7e6b7de0053dfcf09e
Robin Ridler <robin.ridler@gmail.com>
2016-04-04 13:31:41 +0100

    Not available on Solaris

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,18 +1,18 @@
 
-multipackage 'systools' do
-  package_name %w(
-    htop
-    strace
-    ltrace
-    jq
-  )
-end
-
-link '/usr/bin/jq' do
-  to '/usr/bin/jjq'
-end
-
 if node['os'] == 'linux'
+  link '/usr/bin/jjq' do
+    to '/usr/bin/jq'
+  end
+
+  multipackage 'systools' do
+    package_name %w(
+      htop
+      strace
+      ltrace
+      jq
+    )
+  end
+
   bash 'install compilers' do
     code <<-EOH
       sudo dnf -y groupinstall 'C Development Tools and Libraries'
